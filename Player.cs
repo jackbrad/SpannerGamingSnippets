7b219b5e69eb620e7ef639a70bef88a5d965ff2a
jackbrad <jackb@live.com>
2022-03-24 15:28:43 -0400

    Updates

---
+++ Player.cs
@@ -0,0 +1,12 @@
+using System;
+namespace SpannerGamingSnippets
+{
+    public class Player
+    {
+        public string PlayerUUID { get; set; }
+        public string Name { get; set; }
+        public string PictureURL { get; set; }
+        public string Location { get; set; }
+        public string Preferences { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ using Grpc.Core;
 using Grpc.Auth;
 using Google.Apis.Auth.OAuth2;
 
+using Newtonsoft.Json;
+
+
+using SpannerGamingSnippets;
+
+
+
 
 //setup connection string to spanner
 string projectId = "bitfoon-dev";
@@ -17,22 +24,26 @@ string credentialFile = "/Users/bradham/downloads/bitfoon-dev-34cce5fcde89.json"
 string connectionString =$"Data Source=projects/{projectId}/instances/{instanceId}/databases/{databaseId}";
 
 //lets auth with a credential file. 
-GoogleCredential googleCredential = GoogleCredential
-    .FromFile(credentialFile);
+GoogleCredential googleCredential = GoogleCredential.FromFile(credentialFile);
 var credentials = googleCredential.ToChannelCredentials();
 
-// Create connection to Cloud Spanner.
-using (var connection = new SpannerConnection(connectionString,credentials))
-{
-    // Execute a simple SQL statement.
-    var cmd = connection.CreateSelectCommand(@"SELECT ""Hello World"" as test");
-    
-    using (var reader = await cmd.ExecuteReaderAsync())
-    {
-        while (await reader.ReadAsync())
-        {
-            Console.WriteLine(
-                reader.GetFieldValue<string>("test"));
-        }
-    }
-}
+//redis server endpoint
+string endpoint = "localhost";
+var rh = new SpannerGamingSnippets.Relper("endpoint");
+
+//create a player 
+var p = new Player();
+p.Name ="NewbSlayer";
+p.Location = "Cleveland";
+p.PictureURL = "https://giphy.com/gifs/L1VRSg6CslKVZoxWBu";
+p.PlayerUUID = Convert.ToString(Guid.NewGuid);
+p.Preferences = JsonConvert.SerializeObject(new { Value = 108, Welcome = "Hello", GameMode=1 });
+
+
+
+
+
+
+
+
+
